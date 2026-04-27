131252915cb449224c07440811a4c5aa9a18e183
Matheus-MxLa <matheuslacerdamaximiano@gmail.com>
2026-04-27 11:51:12 -0300

    Antes do Label

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+  import { ref } from 'vue';
+  import { listaProdutos } from './data/produtos.js';
+  import ProdutoChild from './components/ProdutoChild.vue'
+  import ButtonChild from './components/ButtonChild.vue';
+
+  const produtos = ref(listaProdutos)
+  const alternando = ref(true)
+  const preco = ref(0)
+
+  function salvarPreco() {
+    alert('Teste')
+  }
+</script>
+
+<template>
+  <div class="container">
+    <h1>Catálogo de Produtos</h1>
+    <div>
+      <ul>
+        <ProdutoChild v-for="produto in produtos"
+          :key="produto.id" :id="produto.id"
+          :nome="produto.nome" :preco="produto.preco"
+          :categoria="produto.categoria"
+        />
+      </ul>
+    </div>
+    <div v-show="alternando">
+      <label>Preço</label>
+      <input type="number" v-model.number="preco">
+      <ButtonChild @clique="salvarPreco()">Salvar</ButtonChild>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProdutoChild.vue
@@ -0,0 +1,15 @@
+<script setup>
+ defineProps(['id', 'nome', 'preco', 'categoria'])
+ import ButtonChild from './ButtonChild.vue';
+</script>
+
+<template>
+  <li>
+    {{ nome }} - R$ {{ preco }} - ({{ categoria }})
+    <ButtonChild>Corrigir Preço</ButtonChild>
+  </li>
+</template>
+
+<style scoped>
+
+</style>
